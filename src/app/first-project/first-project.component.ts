bc1e64db2a2b40cc6f8672727ca2b60a8c50a09f
PCissewski <cissek99@gmail.com>
2022-05-21 15:50:36 +0200

    Added input validation

--- src/app/first-project/first-project.component.ts
+++ src/app/first-project/first-project.component.ts
@@ -11,7 +11,7 @@ export class FirstProjectComponent implements OnInit {
   name: string = "";
   recordAdded: boolean = false;
 
-  emptyRecordMessage: string = "Can't add empty record!";
+  recordMessageWarning: string = "Invalid record format";
   emptyRecordWarning: boolean = false;
 
   constructor(private myService: RecordService) { }
@@ -20,16 +20,24 @@ export class FirstProjectComponent implements OnInit {
   }
 
   onEnter(value: string){
-    this.myService.postAddRecord(value);
-    if (value == "") {
+    
+    if (this.validateValue(value)) {
+      console.log(this.recordMessageWarning)
       this.emptyRecordWarning = true;
       this.recordAdded = false;
       return;
     }
 
+    this.myService.postAddRecord(value);
+
     this.emptyRecordWarning = false;
     this.recordAdded = true;
     this.name = value;
+  }
+
+  validateValue(str: string){
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return str == "" || specialChars.test(str);
   }
 
 }
--- src/app/first-project/service/record.service.ts
+++ src/app/first-project/service/record.service.ts
@@ -1,13 +1,7 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient} from "@angular/common/http";
 
-const httpOptions = {
-    headers: new HttpHeaders({'Content-Type': 'application/json'})
-}
-
-@Injectable({
-    providedIn: 'root'
-})
+@Injectable()
 
 export class RecordService {
     url: string = "https://localhost:7064/Record?record="
@@ -16,10 +10,10 @@ export class RecordService {
 
     postAddRecord(postData: string){
         console.log("To add record: " + postData);
-        return this.http.post(`${this.url+postData}`, postData, httpOptions).subscribe(responseData => {
-            console.log(responseData);
+        return this.http.post(`${this.url+postData}`, {observe: 'response'})
+        .subscribe(response => {
+            console.log(response)
         });
+        
     }
-
-    
 }
